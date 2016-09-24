de1b82f4fd31259e04577c4a6bc7b1e53d5a4fa8
David <davidporter33@icloud.com>
2016-09-24 10:13:25 -0700

    question 2 solved

--- app.js
+++ app.js
@@ -21,9 +21,14 @@ Write a function called multiply() that takes in two numbers as arguments and re
 Test this function by hand in the console to get it working, and when you think it is finished, uncomment the call for the testMultiply() function and see if the test passes.*/
 
 // Write your code here
+var testMultiply = function (x, y) {
+  var multiplyNumber = x * y;
+  return [multiplyNumber, 'The product of ' + x + ' and ' + y + ' is ' + multiplyNumber];
+};
 
-// Here is the test for multiply(); uncomment it to run it
-testMultiply(4,7);
+testMultiply(4, 7);
+
+
 
 // Once you get the test passing, do an a-c-p cycle, synchronize the code between both GitHubs and both laptops, and swap Driver/Navigator roles. Don't forget to create a new branch for your work on the next question!
 
